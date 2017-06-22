31a8c1d8e9593f39dd7fb5391f4ec6a0ea04f72c
Ben Weiss <benweiss@google.com>
2017-06-22 10:47:28 +0100

    Introduce kotlin instant apps sample
    Change-Id: I38f6ed45f6bd64ca800582233d23e1f95564d208

---
+++ hello-kotlin/features/hello/src/main/java/com/instantappsamples/feature/hello/HelloActivity.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.instantappsamples.feature.hello
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * This Activity displays a simple hello world text.
+ */
+class HelloActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+        findViewById(R.id.button).setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://hello-feature.instantappsample.com/goodbye")).apply {
+                `package` = packageName
+                addCategory(Intent.CATEGORY_BROWSABLE)
+            })
+        }
+    }
+}
