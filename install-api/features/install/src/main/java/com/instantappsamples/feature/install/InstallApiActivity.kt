7f49ba68ca29f40cab3ad86ed9d84a107619928e
Ben Weiss <benweiss@google.com>
2019-01-10 16:06:39 +0000

    Migrate install-api to AndroidX

--- install-api/features/install/src/main/java/com/instantappsamples/feature/install/InstallApiActivity.kt
+++ install-api/features/install/src/main/java/com/instantappsamples/feature/install/InstallApiActivity.kt
@@ -3,7 +3,7 @@ package com.instantappsamples.feature.install
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import com.google.android.gms.instantapps.InstantApps
 
