aea04057d2aee2ccd7e5304c808a87cd0f10894c
Ben Weiss <benweiss@google.com>
2019-01-10 15:47:30 +0000

    Migrate analytics to AndroidX

--- analytics/feature/src/main/java/com/example/android/instant/analytics/feature/MainActivity.java
+++ analytics/feature/src/main/java/com/example/android/instant/analytics/feature/MainActivity.java
@@ -16,7 +16,7 @@
 
 package com.example.android.instant.analytics.feature;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
