1e6c734a9c508b46ef5cfe0a8d1a7cebb8a738a1
Ben Weiss <benweiss@google.com>
2019-01-10 15:53:27 +0000

    Migrate cookie-api to AndroidX

--- cookie-api/features/cookie/src/framework/java/com/instantappsamples/feature/cookie/CookieStorageActivity.kt
+++ cookie-api/features/cookie/src/framework/java/com/instantappsamples/feature/cookie/CookieStorageActivity.kt
@@ -17,7 +17,7 @@
 package com.instantappsamples.feature.cookie
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
